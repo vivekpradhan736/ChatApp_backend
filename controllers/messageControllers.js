62bc15477f6ee7c535de214776555757198c2351
Vivek <vivekpradhan736@gmail.com>
2024-06-29 11:23:31 +0530

    new commit

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -2,7 +2,6 @@ const asyncHandler = require("express-async-handler");
 const Message = require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
-const cloudinary = require("cloudinary");
 
 //@description     Get all Messages
 //@route           GET /api/Message/:chatId
@@ -56,32 +55,30 @@ const sendMessage = asyncHandler(async (req, res) => {
   });
 
   const sendAttachments = asyncHandler(async (req, res) => {
-    console.log("req.body", req.body)
-    console.log("req.body.chatId", req.body.chat)
-    const { attachments, content, chat } = req.body;
-    if (!attachments || !chat) {
+    const { public_id, url, content, chatId } = req.body;
+    console.log("public_id",public_id)
+    console.log("url",url)
+    console.log("content",content)
+    console.log("chatId",chatId)
+
+    if (!public_id || !url || !chatId) {
       console.log("Invalid data passed into request");
       return res.sendStatus(400);
     }
-
-    const myCloud = await cloudinary.v2.uploader.upload(attachments, {
-      folder: "chatImages",
-      width: 150,
-      crop: "scale",
-    });
-    console.log("myCloud", myCloud)
-
-    var newMessage = {
-      sender: req.user._id,
-      chat: chat,
-      attachments: [{
-        public_id: result.public_id,
-        url: result.secure_url,
-      }],
-    };
-
-    if (content !== " ") {
-      newMessage.content = content;
+    if(content === " "){
+      var newMessage = {
+        sender: req.user._id,
+        chat: chatId,
+        attachments: [{ public_id, url }],
+      };
+    }
+    else {
+      var newMessage = {
+        sender: req.user._id,
+        content: content,
+        chat: chatId,
+        attachments: [{ public_id, url }],
+      };
     }
 
     try {
@@ -108,31 +105,5 @@ const sendMessage = asyncHandler(async (req, res) => {
       throw new Error(error.message);
     }
   });
-
-  const attachmentsCancel = asyncHandler(async (req, res) => {
-    console.log("Request received to cancel attachment:", req.body);
   
-    const { public_id } = req.body;
-  
-    if (!public_id) {
-      console.log("Invalid data passed into request");
-      return res.sendStatus(400);
-    }
-  
-    try {
-      console.log("Calling Cloudinary API to delete attachment...");
-      cloudinary.v2.uploader.destroy(public_id, function (error, result) {
-        if (error) {
-          console.error("Error during deletion of attachment:", error);
-          return res.status(400).json({ message: "Attachment deletion failed", error: error.message });
-        }
-        console.log("Result of deletion:", result);
-        res.json(result);
-      });
-    } catch (error) {
-      console.error("Error in attachmentsCancel route:", error);
-      res.status(500).json({ message: "Internal Server Error", error: error.message });
-    }
-  });
-  
-  module.exports = { allMessages, sendMessage, sendAttachments, attachmentsCancel };
+  module.exports = { allMessages, sendMessage, sendAttachments };
