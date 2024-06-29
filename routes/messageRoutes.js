fc75a5ef695530d6a86f316ea1ea358bbdffc866
Vivek <vivekpradhan736@gmail.com>
2024-06-29 12:05:28 +0530

    new commit

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,7 +3,6 @@ const {
   allMessages,
   sendMessage,
   sendAttachments,
-  attachmentsCancel,
 } = require("../controllers/messageControllers");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -12,6 +11,5 @@ const router = express.Router();
 router.route("/:chatId").get(protect, allMessages);
 router.route("/").post(protect, sendMessage);
 router.route("/attachment").post(protect, sendAttachments);
-router.route("/attachmentcancel").post(protect, attachmentsCancel);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 const { chats } = require('./data/data');
 const path = require("path");
 const cors = require('cors');
-const cloudinary = require("cloudinary");
 
 dotenv.config({
   path: "./.env",
@@ -29,12 +28,6 @@ app.use(cors({
   },
   credentials: true,
 }));
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 // routes
 app.use('/api/user', userRoutes);
